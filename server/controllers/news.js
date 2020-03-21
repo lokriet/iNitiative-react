0adc36e90a22215dac9931b358eac0a2849cb04b
Evgenia Lazareva <ornaby@gmail.com>
2020-03-21 16:11:26 -0400

    less console logs

--- server/controllers/news.js
+++ server/controllers/news.js
@@ -3,7 +3,6 @@ const News = require('../model/news');
 module.exports.getNews = async (req, res, next) => {
   try {
     const news = await News.find({}, '', { sort: { createdAt: -1 } });
-    console.log(news);
     res.status(200).json(news);
   } catch (error) {
     next(error);
